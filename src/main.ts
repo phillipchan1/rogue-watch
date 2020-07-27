0164e0061e5e311d0e8540cfe3b782bc3a4e873e
Phillip Chan <phil.chan@sce.com>
2020-07-27 13:22:05 -0700

    Get initial screenshot of rogue competition plate

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
+const puppeteer = require('puppeteer')
+
 export class Server {
-  constructor() {
-    console.log('hello world')
+  constructor() {}
+
+  async app() {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(
+      'https://www.roguefitness.com/rogue-calibrated-lb-steel-plates'
+    )
+    const screenshot = await page.screenshot({ path: 'example.png' })
+
+    await browser.close()
   }
 }
 
-module.exports = new Server()
+module.exports = new Server().app()
